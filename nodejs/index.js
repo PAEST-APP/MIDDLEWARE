1b429f236b99d3fb78dfe94c2877ba7b1a1896a9
PAEST-APP <80470160+PAEST-APP@users.noreply.github.com>
2021-04-16 15:34:58 +0200

    Update index.js

--- nodejs/index.js
+++ nodejs/index.js
@@ -33,15 +33,42 @@ function createEvent(studentName, studentEmail, teacherName, teacherEmail, teach
     ],
   }
 
-  return calendar.events.insert(
-    { calendarId: 'primary', resource: event },
-    err => {
-      // Check for errors and log them if they exist.
-      if (err) return console.error('Error Creating Calender Event:', err)
-      // Else log that the event was created.
-      return console.log('Calendar event successfully created.')
-    }
-  )
+  calendar.freebusy.query(
+        {
+            resource: {
+                timeMin: dateStart,
+                timeMax: dateEnd,
+                timeZone: 'Europe/Madrid',
+                items: [{ id: 'primary' }],
+            },
+        },
+        (err, res) => {
+            // Check for errors in our query and log them if they exist.
+            if (err) return console.error('Free Busy Query Error: ', err)
+
+            // Create an array of all events on our calendar during that time.
+            const eventArr = res.data.calendars.primary.busy
+
+            // Check if event array is empty which means we are not busy
+            if (eventArr.length === 0)
+                // If we are not busy create a new calendar event.
+                return calendar.events.insert(
+                    {
+                        calendarId: 'primary',
+                        resource: event,
+                    },
+                    err => {
+                        // Check for errors and log them if they exist.
+                        if (err) return console.error('Error Creating Calender Event:', err)
+                        // Else log that the event was created.
+                        return console.log('Calendar event successfully created. ' + calendarId)
+                    }
+                )
+
+            return console.log(`Sorry I'm busy...`
+            )
+        }
+    )
 
 }
 module.exports = {
