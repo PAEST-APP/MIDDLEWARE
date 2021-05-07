5e664efed1501617758279575bed3d05462aadcf
PAEST-APP <80470160+PAEST-APP@users.noreply.github.com>
2021-05-07 16:23:14 +0200

    Add files via upload

--- horarios_consulta.js
+++ horarios_consulta.js
@@ -2,7 +2,9 @@
 const infoProfe = {
     "codi_persona": 1563,
     "consultes": [
-        "Dimecres 16:00-18:00"
+        "Dimecres 16:00-18:00",
+        "Divendres 16:00-18:00",
+        "Dilluns 16:00-18:00"
     ],
     "despatx": "D6-212",
     "email": "guerrero@ac.upc.edu",
@@ -10,48 +12,67 @@ const infoProfe = {
 }
 
 let date, weekDay;
-consulta=infoProfe.consultes[0];
+let diaConsultas = [];
 
-weekDay = 1; //di de la semana, empieza con domingo
-date = new Date();
-let dateEnd = new Date();
+for (let i = 0; i < infoProfe.consultes.length; i++) {
+    var dia = infoProfe.consultes[i].split(" ");
+    diaConsultas.push(codigoDia(dia[0]));
 
-let currentDay = date.getDay(); // horari d'inici de la consulta
-let distance = weekDay - currentDay;
-date.setDate(date.getDate() + distance);
-date.setHours(12, 0, 0);
-//console.log(date);
-
-dateEnd.setDate(dateEnd.getDate() + distance); // horari de fi de la consulta
-dateEnd.setHours(15, 30, 0);
-//console.log(dateEnd);
-
-let i = false;
-let horaris = [];
-let meetingLength = 30;
-horaris.push(date);
-
-while (!i) { // guarda tots els slots de 30min entre la hora d'inici i fi de consulta del professorat
-    let horari = new Date(horaris[horaris.length - 1].getTime() + meetingLength * 60000);
-    if (horari.getTime() < dateEnd.getTime()) {
-        horaris.push(horari);
-    } else {
-        i = true;
-    }
-}
-//console.log("HORARIS FINALS");
-//console.log(horaris);
-
-let horaris2 = []; // agafa totes les hores disponibles per consulta amb els seus slots,i les passa a la setmana següent
-for (let j = 0; j < 12; j++) {
-    for (let i = 0; i < horaris.length; i++) {
-
-        let date2 = new Date();
-        date2.setDate(horaris[i].getDate() + j*7);
-        date2.setHours(horaris[i].getHours(), horaris[i].getMinutes(), 0);
-        horaris2.push(date2);
-    }
+    var horasConsulta = dia[1].split('-');
+    diaConsultas.push(parseInt(horasConsulta[0].split(':')));
+    diaConsultas.push(parseInt(horasConsulta[1].split(':')));
 }
 
-console.log("NEXT WEEK");
-console.log(horaris2);
+
+let horaris2 = []; 
+
+for (let k = 0; k < diaConsultas.length / 3; k++) {
+
+ weekDay = diaConsultas[k*3]; //dia de la semana
+    date = new Date();
+    let dateEnd = new Date();
+
+    let currentDay = date.getDay(); // horari d'inici de la consulta
+    let distance = weekDay - currentDay;
+    date.setDate(date.getDate() + distance);
+    date.setHours(diaConsultas[k*3+1], 0, 0);
+    //console.log(date);
+
+    dateEnd.setDate(dateEnd.getDate() + distance); // horari de fi de la consulta
+    dateEnd.setHours(diaConsultas[k*3+2], 0, 0);
+    //console.log(dateEnd);
+
+    let i = false;
+    let horaris = [];
+    let meetingLength = 30;
+    horaris.push(date);
+
+    while (!i) { // guarda tots els slots de 30min entre la hora d'inici i fi de consulta del professorat
+        let horari = new Date(horaris[horaris.length - 1].getTime() + meetingLength * 60000);
+        if (horari.getTime() < dateEnd.getTime()) {
+            horaris.push(horari);
+        } else {
+            i = true;
+        }
+    }
+  
+    for (let j = 0; j < 4; j++) {
+        for (let i = 0; i < horaris.length; i++) {
+
+            let date2 = new Date();
+            date2.setDate(horaris[i].getDate() + j * 7);
+            date2.setHours(horaris[i].getHours(), horaris[i].getMinutes(), 0);
+
+            horaris2.push(date2);
+        }
+    }
+     
+}
+
+console.log(horaris2.sort((a,b) => (a.getTime() - b.getTime()))) ;
+
+
+function codigoDia(dia) {
+    dias = ['Diumenge', 'Dilluns', 'Dimarts', 'Dimecres', 'Dijous', 'Divendres', 'Dissabte'];
+    return dias.indexOf(dia);
+}
