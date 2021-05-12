68acc90239acf781a9be5cde84e96136429b0982
adrian-soria-upc <adriansoriamontoya@MacBook-Pro-de-Adrian.local>
2021-05-12 18:28:37 +0200

    get_professor update

--- get_professor.js
+++ get_professor.js
@@ -1,20 +1,20 @@
-function get_professor(id){    
-    var mysql = require('mysql');
+  
+var mysql = require('mysql');
 
-    var con = mysql.createConnection({
-        host: "paedatabase.c6w0dfqhf1bm.eu-west-3.rds.amazonaws.com",
-        user: "PAEmasteruser",
-        password: "0123456789",
-        database: "PAEdatabase"
-    });
+var con = mysql.createConnection({
+    host: "paedatabase.c6w0dfqhf1bm.eu-west-3.rds.amazonaws.com",
+    user: "PAEmasteruser",
+    password: "0123456789",
+    database: "PAEdatabase"
+});
 
-    con.connect(function(err) {
+con.connect(function(err) {
+if (err) throw err;
+con.query("SELECT * FROM directori", function (err, result, fields) {
     if (err) throw err;
-    con.query("SELECT * FROM directori WHERE codi_persona = " + id, function (err, result, fields) {
-        if (err) throw err;
-        console.log(result);
-        return result;
-    });
-    con.end();
-    });
-}
+    console.log(result);
+    return result;
+});
+con.end();
+});
+
